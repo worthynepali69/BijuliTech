036af5c9876fde3e4ef2c0784ea05892a54490ba
worthynepali69 <88470432+worthynepali69@users.noreply.github.com>
2025-11-25 17:01:20 +1300

    Add unittest setup for BijuliSystem
    template so that we can complete it

--- test_app.py
+++ test_app.py
@@ -1,1 +1,13 @@
 #gotta add some test cases mate (it has to be 5)
+import unittest
+from database import Database
+
+# ==============================================================================
+# BIJULI TECH POS - AUTOMATED TEST SUITE
+# ==============================================================================
+
+class TestBijuliSystem(unittest.TestCase):
+
+    def setUp(self):
+        """Run before every test: Setup DB connection"""
+        self.db = Database()
